39b12fa29e9ace11b82d4c0a2f41568613c2e858
mbickell <matthew.bicell@nology.io>
2019-11-26 08:48:35 +0000

    Setup finished

---
+++ src/Containers/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>Test</h1>
+      </>
+    );
+  }
+}
+
+export default App;
