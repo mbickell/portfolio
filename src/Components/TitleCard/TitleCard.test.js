945175e28421b4bb059b138c97df459dace0ff37
Matthew Bickell <matthew.bickell@Nology.local>
2019-12-11 14:04:28 +0000

    project and title card test

---
+++ src/Components/TitleCard/TitleCard.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TitleCard from "./TitleCard";
+import { shallow } from "enzyme";
+
+describe("<TitleCard />", () => {
+  let titleCard;
+  beforeEach(() => {
+    titleCard = shallow(<TitleCard />);
+  });
+
+  test("Renders headers", () => {
+    expect(titleCard.find("h1").length).toBe(1);
+    expect(titleCard.find("h2").length).toBe(1);
+    expect(titleCard.find("h3").length).toBe(2)w;
+  });
+});
